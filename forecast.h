fcb54d23c10d23f4615dc6155c4b2eb249b5cc3d
Guilherme Santana <guilherme.santana@qq.com>
2014-01-10 11:13:58 -0200

    forecast.h Added
    Added a header file for forecast.c.

---
+++ forecast.h
@@ -0,0 +1,8 @@
+#ifndef FORECAST_H_INCLUDED
+#define FORECAST_H_INCLUDED
+
+typedef struct stationData stationData;
+
+double precipitacao(stationData *, double *, int *);
+
+#endif // FORECAST_H_INCLUDED
