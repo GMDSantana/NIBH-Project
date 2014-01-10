8742511e9ad8dbe61487eda8db58103c1823a6a3
GuilhermeSantana <guilherme.santana@qq.com>
2014-01-10 12:50:10 -0200

    Updated path to SWMM in brain.c
    The path to SWMM for interfacing was updated in brain.c.

--- brain.c
+++ brain.c
@@ -16,7 +16,7 @@ int SwmmCall()
 {
     // Nomes dos arquivos setados "manualmente", assim como na previsao.c em SalvaResultados.
     // Isso deve ser acertado durante a fase de sincronizacao.
-    char swmmPath[100] = "\"..\\..\\EPA_SWMM_5.0\\swmm5.exe "; // caminho para o executavel do swmm
+    char swmmPath[100] = "SWMM\\EPA_SWMM_5.0\\swmm5.exe "; // caminho para o executavel do swmm
     char inpFName[50] = "FrutalTeste.inp "; //nome do arquivo de entrada
     char rptFName[50] = "FrutalTesteOut1.rpt "; // nome do arquivo report saida
     char outFName[50] = "FrutalTesteOut2.out\""; // nome do arquivo binario saida
