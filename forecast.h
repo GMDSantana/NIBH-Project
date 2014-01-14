3244049e6b776fef74de71ab2a4529726d78c260
GuilhermeSantana <guilherme.santana@qq.com>
2014-01-14 13:49:48 -0200

    Organising the project
    I separated brain.c and brain.h into five new files: data_acquisition.c, data_acquisition.h, swmm_interfacing.c, swmm_interfacing.h and main.c. I also removed some unused libraries, and then commented each "include" line with itens that are being used by the code in the respective library. And last, I placed in code files and header files (including forecast.c and forecast.h) what should be in each file, and took off what shouldn't. I intend with this update to improve the project's efficiency, maintainability, reusability and readability.

--- forecast.h
+++ forecast.h
@@ -1,8 +1,34 @@
-#ifndef FORECAST_H_INCLUDED
-#define FORECAST_H_INCLUDED
+#ifndef FORECAST_H
+#define FORECAST_H
 
-typedef struct stationData stationData;
+#include <math.h> /* pow, fabs, exp */
+#include <stdio.h> /* FILE, fopen, printf, fprintf, fclose, fseek, ftell, SEEK_END */
+#include <stdlib.h> /* system */
+#include <time.h> /* mktime, time, tm */
 
-double precipitacao(stationData *, double *, int *);
+#include "data_acquisition.h" /* stationData */
 
-#endif // FORECAST_H_INCLUDED
+/// mudar #define para const int/float/double, etc..
+/// checar quais valores podem assumir quais tipos de variaveis
+/* CONSTANTES */
+#define PTRANSF 133.32231202221 // Utilizado para transformar pressao mmHg em Pa
+#define A1 0.0008 //A1 = 8.10^-4 Kg/(m.s2.k3,5)
+#define PN 100000 //Pressao nominal 10^5 Kg/(m.s2)
+#define A 2500000 //Constante da eq. (14) do calor latente A=2,5.10^6 J/Kg
+#define B 2380 //Constante da eq. (14) do calor latente B=2,38.10^3 J/(Kg.K)
+#define CP 1004 //Calor especifico à pressao constante cp=1004 J/(kg.K)
+#define DA 1 //Diferencial de área dA
+#define ALFA 3500 //3500 s-1
+#define TAST 273.15 //T* [ºK] eq 42
+#define PAST 101325 //p* [kg/(m*s2) eq 42
+#define C1 700000 //C1 [kg/(m3.s2)] pag 1593
+#define RV 461 //Constante dos gases para vapor de água [J/(kg.K)] pag 1592
+#define R 287 //J/(K.Kg)
+#define G 9.81 //Aceleração da gravidade m/s2
+#define K 0 //Considerado pag 1602
+#define GAMA 1
+//abstraídos: ifs de tempo/hora // linha/total_linha // altura // parametros de entrada
+
+int PrecForecast(stationData *, double *, int *);
+
+#endif /* FORECAST_H */
